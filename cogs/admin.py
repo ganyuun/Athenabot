07388bb8b52542df1ef6db4a28a9722df2d7b266
Samuel <33796679+orchidalloy@users.noreply.github.com>
2022-01-06 00:05:17 -0300

    Reorganize athenabot into multiple files and ability to update it from github with a command

---
+++ cogs/admin.py
@@ -0,0 +1,105 @@
+import os
+import discord
+from discord.ext import commands
+
+class Admin(commands.Cog):
+    def __init__(self, bot: commands.Bot):
+        self.bot = bot
+
+    @commands.command()
+    @commands.is_owner()
+    async def reload(self, ctx):
+        """Reloads the bot's cogs"""
+        for cog in tuple(self.bot.extensions.keys()):
+            self.bot.reload_extension(cog)
+        await ctx.message.add_reaction('✅')
+
+    @commands.command()
+    @commands.is_owner()
+    async def update(self, ctx):
+        """Updates the bot then reloads the cogs"""
+        # Update files from github
+        stream = os.popen('git pull')
+        output = stream.read()
+        if 'Already up to date' in output:
+            output = ""
+        cogs = tuple(self.bot.extensions.keys())
+        # Existing cogs
+        for cog in cogs:
+            try:
+                self.bot.reload_extension(cog)
+            except commands.ExtensionFailed as e:
+                output += f'\n\n{e}'
+        # New cogs
+        for filename in os.listdir('./cogs'):
+            newcog = f'cogs.{filename[:-3]}'
+            if filename.endswith('.py') and newcog not in cogs:
+                try:
+                    self.bot.load_extension(newcog)
+                except commands.NoEntryPointError:
+                    pass
+                except commands.ExtensionFailed as e:
+                    output += f'\n\n{e}'
+        if output:
+            await ctx.send(f'```{output}```')
+        else:
+            await ctx.message.add_reaction('✅')
+
+    @commands.Cog.listener()
+    async def on_command_error(self, ctx, error: commands.CommandError):
+        if isinstance(error, commands.NotOwner):
+            await ctx.message.add_reaction('❌')
+        else:
+            print(error)
+
+
+class EmbedHelpCommand(commands.HelpCommand):
+    COLOR = discord.Color(int('FCDF99', 16))
+
+    def get_command_signature(self, command):
+        return f'{command.qualified_name} {command.signature}'
+
+    async def send_bot_help(self, mapping):
+        embed = discord.Embed(title='Grey Commands', colour=self.COLOR)
+        for cog, cmds in mapping.items():
+            name = 'No Category' if cog is None else cog.qualified_name
+            filtered = await self.filter_commands(cmds, sort=True)
+            if filtered:
+                value = '\n'.join(f'`{c.name}` - {c.short_doc}' for c in filtered)
+                embed.add_field(name=name, value=value, inline=False)
+
+        await self.get_destination().send(embed=embed)
+
+    async def send_cog_help(self, cog):
+        embed = discord.Embed(title=f'{cog.qualified_name} Commands', colour=self.COLOR,
+                              description=cog.description)
+        filtered = await self.filter_commands(cog.get_commands(), sort=True)
+        for command in filtered:
+            embed.add_field(name=self.get_command_signature(command),
+                            value=command.short_doc or '...', inline=False)
+        await self.get_destination().send(embed=embed)
+
+    async def send_command_help(self, cmd: commands.Command):
+        embed = discord.Embed(title=cmd.qualified_name, colour=self.COLOR, description="")
+        if cmd.help:
+            embed.description += cmd.help
+        if cmd.aliases:
+            embed.add_field(name="Aliases", value=','.join(f'`{c}`' for c in cmd.aliases))
+        await self.get_destination().send(embed=embed)
+
+    async def send_group_help(self, group):
+        embed = discord.Embed(title=group.qualified_name, colour=self.COLOR, description="")
+        if group.help:
+            embed.description += group.help
+        filtered = await self.filter_commands(group.commands, sort=True)
+        for command in filtered:
+            embed.add_field(name=self.get_command_signature(command), value=command.short_doc or '...',
+                            inline=False)
+        await self.get_destination().send(embed=embed)
+
+
+def setup(bot: commands.Bot):
+    cog = Admin(bot)
+    bot.add_cog(cog)
+    bot.help_command = EmbedHelpCommand()
+    bot.help_command.cog = cog
---
+++ cogs/athena.py
@@ -0,0 +1,71 @@
+import discord
+from discord.ext import commands
+
+class AthenaCommands(commands.Cog):
+    def __init__(self, bot: commands.Bot):
+        self.bot = bot
+
+    #ping command
+    @commands.command()
+    async def ping(self, ctx):
+        await ctx.send(f'Pong! {round(self.bot.latency * 1000)}ms')
+
+    #song command
+    @commands.command()
+    async def song(self, ctx):
+        songs = [
+            "https://open.spotify.com/track/6nRJTn59VvyEw333QzxPOb?si=20c64379396e4f78",
+            "https://open.spotify.com/track/1uC1sBpO2Orz049ApZNfxT?si=b73be6589f294332",
+            "https://open.spotify.com/track/2SNUnuFt4eTCEUMHAEmyea?si=17de5148e9a24cb3",
+            "https://open.spotify.com/track/4iUDTgkbosia5aneiGXRXc?si=2e3fad5530a74250",
+            "https://open.spotify.com/track/6pmYiUWpJkTa5Q5sVrF0wi?si=1945943e6de24656",
+            "https://open.spotify.com/track/0gjKbgfWl0NBqdTSblKjCg?si=c8a9bfecd5434ecc",
+            "https://open.spotify.com/track/6oL7mmfbuNMw41ZjUL9whj?si=ff162d5af8fb4b06",
+            "https://www.youtube.com/watch?v=Mj2HFLs-0sE",
+            "https://www.youtube.com/watch?v=DaK7zVoKbl8",
+            "https://open.spotify.com/track/6looCR8dNog0skCIldjlOa?si=860ab5c6d8254924",
+            "https://open.spotify.com/track/1r3dIlrJgbGYmRh3mrzwNe?si=0005ea5fc5ed4d1d",
+            "https://open.spotify.com/track/6zcicpr8OKns6iR7unKZNU?si=9ab80b1452d145aa",
+            "https://open.spotify.com/track/59eFIMQ16qAGVt4NAF6JXH?si=48a5223127c34a10",
+            "https://open.spotify.com/track/7ttt5aIgGcDCWqUPh37swz?si=2bafe22c51a84066",
+            "https://open.spotify.com/track/4zvSIAk10UBilreDozjGI0?si=9c7e78bb4b04450b",
+            "https://open.spotify.com/track/3x8QKvIFop2DQQ4HMdEWKs?si=b2ad0b38017d4a3b",
+            "https://open.spotify.com/track/2Uer7J87AJzKkpcTP4lpGY?si=fdf5a9f5aebe4a3c",
+            "https://open.spotify.com/track/53n8NkFXyiwQ8y5naIyNtq?si=ceeabc5769444e63",
+            "https://open.spotify.com/track/23eNTZIykjVEktFtNUgiTh?si=f2ae0a76f8814a93",
+            "https://open.spotify.com/track/6hg8bNptuX6Iv1WylVfb1W?si=792875383a5e4633",
+        ]
+        await ctx.send(random.sample(songs, 1)[0])
+
+    #Vi quote command
+    @commands.command()
+    async def viquote(self, ctx):
+        quotes = [
+            "Punch first. Ask questions while punching.",
+            "If I want your opinion, I'll beat it out of you.",
+            "They'll never know what hit 'em.",
+            "Piltover's finest.",
+            "If you hit a wall, hit it hard!",
+            "I let my hands do the talking.",
+            "Care for a spot of tea? Or maybe a spot of punch in the face!",
+            "Oh, look at me...~ I'm on the case~.",
+            "We can either do this the hard way or... oh wait, no, there's just the hard way.",
+            "Why can't I get a straight answer? It's always just 'Oh no! Stop hitting me! Ow, my face!'",
+            "Freeze! Or don't, I don't care.",
+            "Vi, hah, stands for violence!",
+            "Vi stands for vicious.",
+            "Vi stands for vice.",
+            "Nice shot, cupcake.",
+            "Hey, Jayce! Power slam!",
+            "Aww, was that supposed to hurt, Cait?",
+            "Piltover enforcer, on the scene!",
+            "Vi, stands for victory!",
+            "I'll chat with cupcake later.",
+            "Ahahah. Cait's still got a sweet tooth for bad cops?",
+            "Piltover enforcer, on the scene!",
+        ]
+        await ctx.send(random.sample(quotes, 1)[0])
+
+def setup(bot: commands.Bot):
+    cog = AthenaCommands(bot)
+    bot.add_cog(cog)
